d0b5b720e2bb995a991e83d0a01cc01acd0ada6a
Julian Thiemann <80831812+Tbird004@users.noreply.github.com>
2025-09-18 16:48:56 +0200

    Added unfinished game prototype

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -1,33 +1,73 @@
-import { Scene } from 'phaser';
+import { GameObjects, Physics, Scene } from 'phaser';
 
 export class Game extends Scene
 {
   camera: Phaser.Cameras.Scene2D.Camera;
   background: Phaser.GameObjects.Image;
-  msg_text : Phaser.GameObjects.Text;
+  player: Phaser.Physics.Arcade.Sprite;
+  ground: Phaser.Physics.Arcade.Sprite;
+  obstacle: Phaser.Physics.Arcade.Sprite;
+  colisionPlayerAndGround: Phaser.Physics.Arcade.Collider;
+  colisionPlayerAndObstacle: Phaser.Physics.Arcade.Collider;
+  isDucked: boolean = false;
+  keyUp: Phaser.Input.Keyboard.Key;
+  keyDown: Phaser.Input.Keyboard.Key;
+  keyLeft: Phaser.Input.Keyboard.Key; //Todo: Delete later
+  keyRight: Phaser.Input.Keyboard.Key; //Todo: Delete later
 
   constructor () {
     super('Game');
   }
 
-  create () {
+  create () {//Todo: Add player movement
+    function gameOver(): void{
+      //this.scene.start("GameOver");
+      console.log("Collide");
+    }
     this.camera = this.cameras.main;
     this.camera.setBackgroundColor(0x00ff00);
 
-    this.background = this.add.image(512, 384, 'background');
-    this.background.setAlpha(0.5);
+    this.background = this.add.image(512, 384, 'gameBackground');
 
-    this.msg_text = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-      fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-      stroke: '#000000', strokeThickness: 8,
-      align: 'center'
-    });
-    this.msg_text.setOrigin(0.5);
+    this.ground = this.physics.add.sprite(512,736,"ground");
+    this.ground.setImmovable(true);
 
-    this.input.once('pointerdown', () => {
+    this.obstacle = this.physics.add.sprite(512,688,"obstacle");
+    this.obstacle.setImmovable(true);
 
-      this.scene.start('GameOver');
+    this.player = this.physics.add.sprite(64,64,"playerIdle");
+    this.player.body?.setSize(32,64,false);
+    this.player.setOrigin(0.5,1);
+    this.player.setCollideWorldBounds(true);
+    this.player.setGravityY(500);
 
-    });
+    this.colisionPlayerAndGround = this.physics.add.collider(this.player,this.ground);
+    this.colisionPlayerAndObstacle = this.physics.add.collider(this.player,this.obstacle,gameOver());
+
+    this.keyUp = this.input.keyboard.addKey("W");
+    this.keyDown = this.input.keyboard.addKey("S");
+    this.keyLeft = this.input.keyboard.addKey("A"); //Todo: delete later
+    this.keyRight = this.input.keyboard.addKey("D"); //Todo: delete later
+  }
+  update (){
+    if(this.keyDown.isDown && this.player.body?.touching.down){
+      this.player.body?.setSize(32,32,false);
+      this.player.setTexture("playerDucking");
+      this.player.setVelocityX(0);
+      this.isDucked = true;
+    } else if(this.keyDown.isUp  && this.isDucked){
+      this.player.body?.setSize(32,64,false);
+      this.player.setTexture("playerIdle");
+      this.isDucked = false;
+    } else if(this.keyRight.isDown){
+      this.player.setVelocityX(160); 
+    } else if(this.keyLeft.isDown){
+      this.player.setVelocityX(-160);
+    } else {
+      this.player.setVelocityX(0);
+    }
+    if(this.keyUp.isDown && this.player.body?.touching.down){
+      this.player.setVelocityY(-330);
+    }
   }
 }
