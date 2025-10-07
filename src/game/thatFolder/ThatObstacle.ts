74225d6329c403a9a5cb0f526f6c15869045e77b
Julian Thiemann <80831812+Tbird004@users.noreply.github.com>
2025-10-07 14:36:17 +0200

    improved hitboxes & sprite scaling
    hitboxes and sprite scales of obstacles and the player are now more equal / proportionate in size and hitboxes are now more "fair" towards the player

--- src/game/thatFolder/ThatObstacle.ts
+++ src/game/thatFolder/ThatObstacle.ts
@@ -2,33 +2,73 @@ import {Scene} from "phaser"
 import {globalConsts} from '../main.ts';
 
 export enum obstacleType {
-  GROUND = "GROUND",
-  AIR = "AIR",
+  BIRDBLUE = "BIRDBLUE",
+  BIRDPINK = "BIRDPINK",
+  SNOWMAN = "SNOWMAN",
+  ROCKS = "ROCKS",
   MARKER = "MARKER"
 }
 
-export const viableObstacles: obstacleType[] = [obstacleType.AIR, obstacleType.GROUND];
+export const viableObstacles: obstacleType[] = [obstacleType.BIRDBLUE, obstacleType.BIRDPINK, obstacleType.SNOWMAN, obstacleType.ROCKS];
 
 interface obstacleProperties {
   y: () => number;
-  spriteVariants: string[],
+  sprite: string,
+  width: number,
+  height: number,
+  offsetX: number,
+  offsetY: number,
+  scale: number,
   weight: number
 }
 
 const obstaclePropertiesMap: Record<obstacleType, obstacleProperties> = {//TODO: change weight config
-  [obstacleType.GROUND]: {
-    y: () => globalConsts.getRandomInt(globalConsts.gameHeight - 80, globalConsts.gameHeight - 80),
-    spriteVariants: ["stone", "snowman"],
+  [obstacleType.BIRDBLUE]: {
+    y: () => globalConsts.getRandomInt(globalConsts.gameHeight * 0.4, globalConsts.gameHeight * 0.8),
+    sprite: "birdBlue",
+    width: 16,
+    height: 6,
+    offsetX: 8,
+    offsetY: 14,
+    scale: 2,
+    weight: 100},
+    [obstacleType.BIRDPINK]: {
+    y: () => globalConsts.getRandomInt(globalConsts.gameHeight * 0.4, globalConsts.gameHeight * 0.8),
+    sprite: "birdPink",
+    width: 16,
+    height: 6,
+    offsetX: 6,
+    offsetY: 6,
+    scale: 2,
+    weight: 100},
+  [obstacleType.SNOWMAN]: {
+    y: () => globalConsts.gameHeight - 96,
+    sprite: "snowman",
+    width: 6,
+    height: 12,
+    offsetX: 4,
+    offsetY: 3,
+    scale: 4,
     weight: 100
   },
-  [obstacleType.AIR]: {
-    y: () => globalConsts.getRandomInt(32, globalConsts.gameHeight * 0.5),
-    spriteVariants: ["birdBlue", "birdPink"],
+  [obstacleType.ROCKS]: {
+    y: () => globalConsts.gameHeight - 90,
+    sprite: "stone",
+    width: 164,
+    height: 100,
+    offsetX: 50,
+    offsetY: 70,
+    scale: 0.5,
     weight: 100
   },
   [obstacleType.MARKER]: {
     y: () => globalConsts.gameHeight,
-    spriteVariants: ["player"],
+    sprite: "player",
+    width: 0,
+    height: 0,
+    offsetX: 0,
+    offsetY: 0,
+    scale: 1,
     weight: 0
   }
 }
@@ -43,33 +83,14 @@ export class ThatObstacle {
   constructor(type: obstacleType, x: number, currentScene: Scene, marker: boolean) {
     this.x = x;
     this.y = obstaclePropertiesMap[type].y();
-    this.image = Phaser.Utils.Array.GetRandom(obstaclePropertiesMap[type].spriteVariants);
+    this.image = obstaclePropertiesMap[type].sprite;
     this.scene = currentScene;
 
     this.sprite = this.scene.physics.add.sprite(this.x, this.y, !marker ? this.image : "");
     this.sprite.setAlpha(!marker ? 1 : 0);
-
-    switch (this.image) {
-      case("rocks"): {
-        this.sprite.setScale(0.6);
-        this.sprite.setCircle(64);
-        break;
-      }
-      case("snowman"): {
-        this.sprite.setScale(4);
-        this.sprite.setBodySize(16, 32);
-        break;
-      }
-      case("birdBlue"): {
-        this.sprite.setScale(2);
-        this.sprite.setBodySize(40, 28);
-        break;
-      }
-      case("birdPink"): {
-        this.sprite.setScale(2);
-        this.sprite.setBodySize(46, 32);
-        break;
-      }
-    }
+    
+    if(!marker) this.sprite.setBodySize(obstaclePropertiesMap[type].width,obstaclePropertiesMap[type].height);
+    this.sprite.setScale(obstaclePropertiesMap[type].scale);
+    this.sprite.setOffset(obstaclePropertiesMap[type].offsetX,obstaclePropertiesMap[type].offsetY);
   }
 }
